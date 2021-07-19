703fdd92b5a36f0b09689be80c85167a0519b847
remace <tauvel.remi@gmail.com>
2021-07-19 23:53:14 +0200

    finished google maps API request

--- API/GMapsAPI.py
+++ API/GMapsAPI.py
@@ -4,10 +4,14 @@ import requests
 
 class GMapsAPI:
     def __init__(self):
-        self.req = {}
-        self.api_url = "https://maps.googleapis.com/maps/api/geocode/json"
-        self.req['api_key'] = config.GOOGLE_MAPS_KEY
+        self.params = {}
+        self.api_url = "https://maps.googleapis.com/maps/api/geocode/json?"
+        self.params['key'] = config.GOOGLE_MAPS_KEY
 
     def get_location(self, sentence):
-        self.req['address'] = sentence
-        return requests.post(self.api_url, data=self.req)
+        self.params['address'] = sentence
+        return requests.get(self.api_url, params=self.params).json()
+
+if __name__ == '__main__':
+    gm = GMapsAPI()
+    print(gm.get_location("connais palais idéal facteur cheval"))
