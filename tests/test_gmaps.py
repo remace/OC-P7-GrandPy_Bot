59a733bb99e7ae1582a78cf6ec8140f60def1542
remace <tauvel.remi@gmail.com>
2021-07-20 01:18:27 +0200

    formatted response with only the useful data in it

--- tests/test_gmaps.py
+++ tests/test_gmaps.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from API import GMapsAPI
 from tests import constants
 import urllib.request
@@ -6,7 +8,7 @@ import json
 from unittest import TestCase
 
 
-class Test_GMapsAPI():
+class Test_GMapsAPI:
 
     def setup_method(self):
         self.gm = GMapsAPI.GMapsAPI()
@@ -14,8 +16,8 @@ class Test_GMapsAPI():
     def teardown_method(self):
         self.gm = None
 
-    def test_http_result(self, monkeypatch):
-        results = constants.gmaps_answer
+    def test_get_location(self, monkeypatch):
+        results = constants.GMAPS_ANSWER
 
         def mock_return():
             return BytesIO(json.dumps(results).encode())
@@ -23,3 +25,7 @@ class Test_GMapsAPI():
         monkeypatch.setattr(urllib.request, 'urlopen', mock_return)
 
         TestCase().assertDictEqual(self.gm.get_location("connais palais idéal facteur cheval"), results)
+
+    def test_get_useful_data_from_response(self):
+        self.gm.get_location("connais palais facteur cheval")
+        assert self.gm.get_useful_data_from_response() == constants.USEFUL_DATA
