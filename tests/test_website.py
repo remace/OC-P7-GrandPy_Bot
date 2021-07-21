3ae692755c3e695895871d1e260257cb746aec34
remace <tauvel.remi@gmail.com>
2021-07-21 15:44:40 +0200

    fixed testing API

--- tests/test_website.py
+++ tests/test_website.py
@@ -1,16 +1,22 @@
 import requests
 from tests import constants
 
-
 class Test_Views:
 
     def setup_method(self):
+        self.url = "http://127.0.0.1:5000/AskGrandPy/?"
         self.params = {
             'sentence': 'connais tu le palais idéal du facteur cheval?'
         }
 
+
     def test_grandpy_api(self):
+
         result = {'maps_info': constants.USEFUL_DATA,
-                  'wiki_info': constants.WIKIPEDIA_INTRO}
-        rep = requests.get('http://127.0.0.1:5000/AskGrandpy/', self.params).json()
+                  'wiki_info': {
+                      'intro': constants.WIKIPEDIA_INTRO["query"]['pages']['332154']['extract'],
+                      'title': constants.WIKIPEDIA_INTRO["query"]['pages']['332154']['title']
+                  }
+                  }
+        rep = requests.get(self.url, self.params).json()
         assert rep == result
