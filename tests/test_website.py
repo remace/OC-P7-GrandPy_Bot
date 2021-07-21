7dfca1504ce8a2cab1ef31662b2ad9c1308a4fee
remace <tauvel.remi@gmail.com>
2021-07-21 02:25:42 +0200

    fixed testing API with pytest: no __init__(), setup_method() instead

--- tests/test_website.py
+++ tests/test_website.py
@@ -1,13 +1,15 @@
 import requests
+from tests import constants
+
 
 class Test_Views:
 
-    def __init__(self):
-        self.params={
-            'sentence': 'connais tu le palais du facteur cheval?'
+    def setup_method(self):
+        self.params = {
+            'sentence': 'connais tu le palais idéal du facteur cheval?'
         }
 
-    def test_API_Route(self):
+    def test_grandpy_api(self):
         result = {'maps_info': constants.USEFUL_DATA,
                   'wiki_info': constants.WIKIPEDIA_INTRO}
         rep = requests.get('http://127.0.0.1:5000/AskGrandpy/', self.params).json()
