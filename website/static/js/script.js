6d6b0727c3009a67b5be6149d22939779f44fc1f
remace <tauvel.remi@gmail.com>
2021-07-23 01:18:39 +0200

    added maps displaying

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -20,38 +20,59 @@ function send_data(){
     let params={
         'sentence': INPUT.value
     }
+    create_question_div(INPUT.value)
+    INPUT.value=''
     
     fetch('http://127.0.0.1:5000/AskGrandPy/?sentence='+params.sentence)
         .then(res => res.json())
         .then(data => addNewDiv(data))
 };
 
+function create_question_div(question){
+    let question_div = document.createElement('div')
+    question_div.classList.add('question_div')
+    question_div.innerHTML="<p>"+ question + "</p>"
+    ANSWER_CONTAINER.appendChild(question_div)
+    ANSWER_CONTAINER.scrollTop = ANSWER_CONTAINER.scrollHeight
+}
+
+
+
 function addNewDiv(data){
-    console.log(data)
-    
     let gmaps_div =  create_google_maps_div(data)
-    gmaps_div.classList.add('google_maps_container')
-    
+    let gmaps_container = document.createElement('div')
+    gmaps_container.classList.add('google_maps_container')
+    gmaps_container.appendChild(gmaps_div)
+
     let wikipedia_div  = create_wiki_div(data)
     wikipedia_div.classList.add('wikimedia_container')
 
     let answer = document.createElement('div', { class:'answer'})
     answer.classList.add('answer')
-    answer.appendChild(gmaps_div)
+    answer.appendChild(gmaps_container)
     answer.appendChild(wikipedia_div)
-
     ANSWER_CONTAINER.appendChild(answer)
-    
+    ANSWER_CONTAINER.scrollTop = ANSWER_CONTAINER.scrollHeight
 }
 
 function create_google_maps_div(data){
-    let div = document.createElement('div')
-    let title = "gmaps_answer"
-    let latitude = data.maps_info.results.geometry.location.lat
-    let longitude = data.maps_info.results.geometry.location.lng
-    let content = "<h3>"+title+"</h3><p>lat: "+latitude+"</p><p>lng:"+longitude+"</p>"
-    div.innerHTML = content
-    return (div)
+    let card_div = document.createElement('div')
+    card_div.classList.add('gmap')
+    coords = data.maps_info.results.geometry.location
+
+    map = new google.maps.Map(card_div, {
+        center: coords,
+        zoom: 10,
+        gestureHandling: "cooperative",
+    });
+    
+    const marker = new google.maps.Marker({
+        position: coords,
+        map: map,
+    });
+
+    return card_div
+
 }
 
 function create_wiki_div(data){
@@ -59,7 +80,6 @@ function create_wiki_div(data){
     let title = data.wiki_info.title
     let extract = data.wiki_info.intro
     let link_url = "https://fr.wikipedia.org/wiki/"+title.replace(/ /g,'_')
-    console.log(link_url)
     let content = "<h3>"+title+"</h3><p>"+extract+"</p><p><a href="+ link_url +">lien wikipedia</a></p>"
     div.innerHTML = content   
     return (div)
--- website/views.py
+++ website/views.py
@@ -8,12 +8,13 @@
 
 from flask import Flask, render_template, request
 from libs.GrandPy import GrandPy
+import config
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', google_api_key = config.GOOGLE_MAPS_KEY)
 
 
 @app.route('/AskGrandPy/', methods=['POST', 'GET'])
