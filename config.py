6cde0c16198b0539b9bb39d963d42a82675ee10e
remace <tauvel.remi@gmail.com>
2021-08-02 08:38:43 +0200

    adjustment

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 import os
+import API_Keys
 
-GOOGLE_MAPS_KEY = os.environ.get('GOOGLE_MAPS_KEY')
+if os.environ.get('GOOGLE_MAPS_KEY') is None:
+    GOOGLE_MAPS_KEY = os.environ.get('GOOGLE_MAPS_KEY')
+else:
+    GOOGLE_MAPS_KEY = API_Keys.GOOGLE_MAPS_KEY
