0fffefa3605daa57ff39ecd39b874264d0325a56
Rémi TAUVEL <tauvel.remi@gmail.com>
2022-04-04 02:35:12 +0200

    began fixing tests: data change from google maps

--- tests/test_gmaps.py
+++ tests/test_gmaps.py
@@ -1,6 +1,6 @@
 from libs.API import GMapsAPI
 from tests import constants
-import urllib.request
+import requests
 from io import BytesIO
 import json
 
@@ -15,23 +15,44 @@ class Test_GMapsAPI:
 
     def test_get_location(self, monkeypatch):
         results = constants.GMAPS_ANSWER
-        def mock_return():
+        
+        def mock_return(*args, **kwargs):
+            results = constants.GMAPS_ANSWER
             return BytesIO(json.dumps(results).encode())
 
-        monkeypatch.setattr(urllib.request, 'urlopen', mock_return)
+        monkeypatch.setattr(requests, 'get', mock_return)
+
         assert self.gm._get_location("connais palais idéal facteur cheval") == results
 
     def test_get_location_not_existing(self, monkeypatch):
         results = constants.GMAPS_ANSWER_ZERO_RESULT
-        def mock_return():
+        def mock_return(*args, **kwargs):
+            results = constants.GMAPS_ANSWER_ZERO_RESULT
             return BytesIO(json.dumps(results).encode())
 
-        monkeypatch.setattr(urllib.request, 'urlopen', mock_return)
+        monkeypatch.setattr(requests, 'get', mock_return)
         assert self.gm._get_location('pmquhvb^quehgtbùôquetngùO') == results
 
-    def test_get_useful_data_from_response(self):
+    def test_get_useful_data_from_response(self, monkeypatch):
+        results = constants.USEFUL_DATA
+        def mock_return(*args, **kwargs):
+            results = constants.USEFUL_DATA
+            return BytesIO(json.dumps(results).encode())
+
+        monkeypatch.setattr(requests, 'get', mock_return)
+
         self.gm._get_location("connais palais facteur cheval")
         assert self.gm._get_useful_data_from_response() == constants.USEFUL_DATA
 
-    def test_search(self):
-        assert self.gm.search("connais palais facteur cheval") == constants.USEFUL_DATA
+    def test_search(self, monkeypatch):
+
+        results = constants.USEFUL_DATA
+
+        def mock_return(*args, **kwargs):
+            search_result = constants.USEFUL_DATA
+            return BytesIO(json.dumps(search_result).encode())
+
+        monkeypatch.setattr(requests, 'get', mock_return)
+
+
+        assert self.gm.search("connais palais facteur cheval") == results
